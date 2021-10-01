d3b7838e88fcd19660eb07cf2667a620cb054711
MenahiShayan <shayan1232001@yahoo.co.in>
2021-10-01 17:25:31 +0530

    scene test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+import { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ScrollMagic from 'scrollmagic';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        var controller = new ScrollMagic.Controller();
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: '#header',
+        })
+            .setPin('#logo'); // the element we want to pin
+
+        // Add Scene to ScrollMagic Controller
+        controller.addScene(scene);
+
+        var slides = document.querySelectorAll("section.content");
+
+        for (var i = 0; i < slides.length; i++) {
+            new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+                .setPin(slides[i], { pushFollowers: false })
+                .addTo(controller);
+        }
+    }, [])
+
+    return (
+        <div className="App">
+            <section className="App-header" id="header" >
+                <img src={logo} className="App-logo" alt="logo" id="logo" />
+                {
+                    [...Array(3)].map((a, i) => (
+                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
+                    ))
+                }
+            </section>
+            <section className="content" style={{ backgroundColor: 'white' }}>
+                <img src={logo} className="App-logo" alt="logo" />
+                {
+                    [...Array(3)].map((a, i) => (
+                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
+                    ))
+                }
+            </section>
+            <section  className="content" style={{ backgroundColor: 'grey' }}>
+                <img src={logo} className="App-logo" alt="logo" />
+                {
+                    [...Array(3)].map((a, i) => (
+                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
+                    ))
+                }
+            </section>
+            <section  className="content" style={{ backgroundColor: 'white' }}>
+                <img src={logo} className="App-logo" alt="logo" />
+                {
+                    [...Array(3)].map((a, i) => (
+                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
+                    ))
+                }
+            </section>
+        </div>
+    );
 }
 
 export default App;
