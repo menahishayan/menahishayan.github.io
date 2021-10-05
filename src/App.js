672bf8e21217ae4e1cc2723071a5ad913c920b49
MenahiShayan <shayan1232001@yahoo.co.in>
2021-10-05 18:04:25 +0530

    title

--- src/App.js
+++ src/App.js
@@ -1,67 +1,37 @@
-import { useRef, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ScrollMagic from 'scrollmagic';
+import { Fragment } from 'react';
+import { Controller, Scene } from 'react-scrollmagic'
+import './App.css'
 
 function App() {
-    useEffect(() => {
-        var controller = new ScrollMagic.Controller();
-
-        var scene = new ScrollMagic.Scene({
-            triggerElement: '#header',
-        })
-            .setPin('#logo'); // the element we want to pin
-
-        // Add Scene to ScrollMagic Controller
-        controller.addScene(scene);
-
-        var slides = document.querySelectorAll("section.content");
-
-        for (var i = 0; i < slides.length; i++) {
-            new ScrollMagic.Scene({
-                triggerElement: slides[i]
-            })
-                .setPin(slides[i], { pushFollowers: false })
-                .addTo(controller);
-        }
-    }, [])
-
     return (
-        <div className="App">
-            <section className="App-header" id="header" >
-                <img src={logo} className="App-logo" alt="logo" id="logo" />
-                {
-                    [...Array(3)].map((a, i) => (
-                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
-                    ))
-                }
-            </section>
-            <section className="content" style={{ backgroundColor: 'white' }}>
-                <img src={logo} className="App-logo" alt="logo" />
-                {
-                    [...Array(3)].map((a, i) => (
-                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
-                    ))
-                }
-            </section>
-            <section  className="content" style={{ backgroundColor: 'grey' }}>
-                <img src={logo} className="App-logo" alt="logo" />
-                {
-                    [...Array(3)].map((a, i) => (
-                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
-                    ))
-                }
-            </section>
-            <section  className="content" style={{ backgroundColor: 'white' }}>
-                <img src={logo} className="App-logo" alt="logo" />
-                {
-                    [...Array(3)].map((a, i) => (
-                        <div key={i} style={{ margin: 50 }}>Lorem Ipsum dolor sit amet</div>
-                    ))
-                }
-            </section>
-        </div>
-    );
+        <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
+            <Scene pin>
+                <section className="slide-1">
+                    <h1 className="main-title">Menahi Shayan</h1>
+                </section>
+            </Scene>
+            <Scene pin>
+                <div style={{position:'relative'}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="slide-1-wave-container">
+                        <path className="slide-1-wave" fill="#fff" fill-opacity="1" d="M0,192L48,202.7C96,213,192,235,288,245.3C384,256,480,256,576,240C672,224,768,192,864,170.7C960,149,1056,139,1152,138.7C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                    </svg>
+                    <section className="slide-2">
+                        <h1>Lorem</h1>
+                    </section>
+                </div>
+            </Scene>
+
+            {
+                ['white', 'grey', 'white', 'grey'].map((c, i) => (
+                    <Scene key={i} pin>
+                        <section style={{ backgroundColor: c }}>
+                            <h1>Slide {i + 1}</h1>
+                        </section>
+                    </Scene>
+                ))
+            }
+        </Controller>
+    )
 }
 
 export default App;
