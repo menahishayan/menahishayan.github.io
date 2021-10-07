26f70cf55560d5641aaceb4b4166a54507a7e845
MenahiShayan <shayan1232001@yahoo.co.in>
2021-10-07 15:22:15 +0530

    slide 2 n 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Controller, Scene } from 'react-scrollmagic'
 import { Tween, SplitChars, ScrollTrigger, Timeline } from 'react-gsap';
 import './App.css'
-import { Fragment } from 'react';
-
+import heartLogo from './images/heart.png'
 function App() {
     return (
         <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
@@ -30,24 +29,53 @@ function App() {
                         <path fill="#fff" fill-opacity="1" d="M0,192L48,202.7C96,213,192,235,288,245.3C384,256,480,256,576,240C672,224,768,192,864,170.7C960,149,1056,139,1152,138.7C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                     </svg>
                     <section className="slide-2">
-                        <ScrollTrigger start="50px center" end="600px center" scrub={0.1} >
-                            <Tween from={{ x: '-100vw',transform:'scale(0.4)' }} to={{transform:'scale(1.2)'}} ease="back.out(1.7)">
-                                <div className="slide-2-statement" style={{ marginTop: '20vh', marginLeft: '10vw' }}>Driven by <span className="tg-pink">Passion,</span></div>
-                            </Tween>
-                            <Tween from={{ x: '80vw',transform:'scale(0.4)' }} to={{transform:'scale(1.2)'}} ease="back.out(0.8)">
-                                <div className="slide-2-statement" style={{ marginRight: '19vw' }}>Fueled by <span className="tg-yellow">Curiosity,</span></div>
-                            </Tween>
-                            <Tween from={{ x: '-100vw',transform:'scale(0.4)' }} to={{transform:'scale(1.2)'}} ease="back.out(1.2)">
-                                <div className="slide-2-statement" style={{ marginLeft: '15vw' }}>And Inspired by <span className="tg-purple">Innovation.</span></div>
-                            </Tween>
+                        <ScrollTrigger start="50px center" end="600px center" scrub={0.5} >
+                            <Timeline
+                                target={
+                                    <div className="slide-2-statement" style={{ marginTop: '20vh', marginLeft: '10vw' }}>Driven by <span className="tg-pink">Passion,</span></div>
+                                }>
+                                <Tween from={{ x: '-100vw', transform: 'scale(0.4)' }} to={{ transform: 'scale(1.2)' }} ease="back.out(1.7)" />
+                                <Tween to={{ transform: 'scale(1)' }} />
+                                <Tween to={{ transform: 'scale(0.4)', opacity: 0 }} />
+                            </Timeline>
+                            <Timeline
+                                target={
+                                    <div className="slide-2-statement" style={{ marginRight: '19vw' }}>Fueled by <span className="tg-yellow">Curiosity,</span></div>
+                                }>
+                                <Tween from={{ x: '80vw', transform: 'scale(0.4)' }} to={{ transform: 'scale(1.2)' }} ease="back.out(0.8)" />
+                                <Tween to={{ transform: 'scale(1)' }} />
+                                <Tween to={{ transform: 'scale(0.4)', opacity: 0 }} />
+                            </Timeline>
+                            <Timeline
+                                target={
+                                    <div className="slide-2-statement" style={{ marginLeft: '15vw' }}>And Inspired by <span className="tg-purple">Innovation.</span></div>
+                                }>
+                                <Tween from={{ x: '-100vw', transform: 'scale(0.4)' }} to={{ transform: 'scale(1.2)' }} ease="back.out(1.2)" />
+                                <Tween to={{ transform: 'scale(1)' }} />
+                                <Tween to={{ transform: 'scale(0.4)', opacity: 0 }} />
+                            </Timeline>
                         </ScrollTrigger>
                     </section>
                 </div>
             </Scene>
             <Scene pin>
                 <section className="slide-3">
-                    <span>With a love for </span>
-                    <span className="slide-3-statement">automating, tinkering, and simply making things better.</span>
+                    <div className="slide-3-pretext">
+                        with a
+                        <ScrollTrigger start="-300vh center" end="70vh center" scrub={1} markers>
+                            <Timeline
+                                target={
+                                    <img src={heartLogo} alt="love" style={{ height: 60, width: 60, verticalAlign: 'middle', marginLeft: 10, marginRight: 10 }} />
+                                }>
+                                <Tween to={{ transform: 'scale(1.3)', marginLeft:20, marginRight:20 }} />
+                                <Tween to={{ transform: 'scale(1)', marginLeft: 10, marginRight: 10 }} />
+                                <Tween to={{ transform: 'scale(1.3)', marginLeft:20, marginRight:20 }} />
+                                <Tween to={{ transform: 'scale(1)', marginLeft: 10, marginRight: 10 }} />
+                            </Timeline>
+                        </ScrollTrigger>
+                        for
+                    </div>
+                    {/* <span className="slide-3-statement">automating, tinkering, and simply making things better.</span> */}
                 </section>
             </Scene>
 
