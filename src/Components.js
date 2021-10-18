db1d4f6b5b0823a55734053ac5c7e536dc0a459e
MenahiShayan <shayan1232001@yahoo.co.in>
2021-10-18 16:02:02 +0530

    scene 5

--- src/Components.js
+++ src/Components.js
@@ -5,6 +5,8 @@ import raspberry from './images/raspberry.svg'
 import reactlogo from './images/react.svg'
 import wslogo from './images/websocket.svg'
 import python from './images/python.svg'
+import pypi from './images/pypi.svg'
+import github from './images/github.svg'
 import linux from './images/tux.svg'
 
 const _techBadges = {
@@ -12,6 +14,8 @@ const _techBadges = {
     'TensorFlow': { src: tensorflow, color: 'rgba(229,91,44,1)' },
     'Websockets': { src: wslogo, color: 'rgba(254,102,0,1)' },
     'Python': { src: python, color: 'rgba(254,209,65,1)' },
+    'PyPI': { src: pypi, color: 'rgba(254,209,65,1)', zoom: 1.4 },
+    'GitHub': { src: github, color: 'rgba(0,0,0,1)'},
     'Linux': { src: linux, color: 'rgba(0,0,0,1)' },
     'React Native': { src: reactlogo, color: 'rgba(94,218,250,1)', zoom: 1.7 }
 }
--- src/Scene5.js
+++ src/Scene5.js
@@ -5,12 +5,14 @@ import { TextPlugin } from 'gsap/TextPlugin';
 import './App.css'
 import { Fragment } from 'react';
 
+import { techBadges } from './Components'
+
 gsap.registerPlugin(TextPlugin);
 
 const Scene5 = () => (
     <Scene pin>
         <section className="slide-5" style={{ background: 'linear-gradient(315deg, #2d3436 0%, #000000 74%)', paddingTop: '5vh' }}>
-            <ScrollTrigger start="150px center" end="2000px center" scrub={0.7} markers>
+            <ScrollTrigger start="150px center" end="2500px center" scrub={0.7} markers>
                 <Timeline
                     target={
                         <Fragment>
@@ -21,7 +23,16 @@ const Scene5 = () => (
                             <div className="slide-3-pretext" style={{ textAlign: 'left', marginLeft: '35vw', marginBottom: '0', color: '#555' }}>or...<br /><br /><br /></div>
                             <div className="slide-3-statement" style={{ textAlign: 'left', marginLeft: '35vw', fontSize: 70 }}>Building A Popular<br />Home Automation API</div>
                             <img src="./images/box.png" alt="package box" className="package-box" />
-                            <div className="slide-3-statement" style={{ textAlign: 'center',  fontSize: 70, position:'absolute',top:'22vh',width:'100vw',color:'wheat' }}>HomeScript</div>
+                            <div className="slide-3-statement" style={{ textAlign: 'center', fontSize: 70, position: 'absolute', top: '22vh', width: '100vw', color: 'wheat', paddingLeft: '3vw' }}>HomeScript</div>
+                            <div className="para" style={{ position: 'absolute', top: '35vh', left: 0, textAlign: 'center', width: '100vw', color: 'lightgray', fontWeight: 'bold' }}>Your home just got an API.</div>
+                            <div className="para" style={{ position: 'absolute', top: '45vh', left: 0, textAlign: 'center', width: '50vw', color: 'lightgray', paddingLeft: '25vw', paddingRight: '25vw' }}>
+                                HomeScript is a robust API that allows command-line control of your smart home accessories.
+                                This popular script serves as the underlying framework to run home automations and opens your home to a world of scripting capabilites!<br /><br />
+                                With command-line controls, a fully importable API, group functions, intelligent support for loosely-typed commands and EasyMatch referencing, HomeScript is the simplest and most robust way of automating your HomeKit installation!
+                            </div>
+                            <div style={{ position: 'absolute', bottom: '300vh', flexDirection: 'row', display: 'flex', paddingLeft: '40vw', filter: 'drop-shadow(0 0 0.8rem rgba(255,206,252,0.9))' }}>
+                                {techBadges(['Python', 'GitHub', 'PyPI'])}
+                            </div>
                         </Fragment>
                     }
                     labels={Array(30).map((_, a) => { return { label: a + '', position: a } })}
@@ -63,13 +74,17 @@ const Scene5 = () => (
                     <Tween to={{ delay: 1, opacity: 0 }} target={3} position="13" />
                     <Tween to={{ delay: 1, opacity: 0 }} target={1} position="13" />
 
-                    <Tween from={{ x: '-10vw', y: '10vh', opacity: 0 }} to={{x:'-5vw', y: '0vh', opacity: 1}} target={6} position="15" ease="back.out(4)"/>
+                    <Tween from={{ x: '-10vw', y: '10vh', opacity: 0 }} to={{ x: '-5vw', y: '0vh', opacity: 1 }} target={6} position="15" ease="back.out(4)" />
                     <Tween to={{ delay: 0.8, x: '-5vw', y: '30vh', transform: 'scale(0.3)', opacity: 0 }} target={2} position="15" ease="elastic.out(0.1, 0.2)" />
                     <Tween to={{ delay: 1.2, x: '-45vw', y: '55vh', transform: 'scale(0.2)', opacity: 0 }} target={4} position="15" ease="elastic.out(0.1, 0.2)" />
                     <Tween to={{ delay: 1, x: '-48vw', y: '35vh', transform: 'scale(0.1)', opacity: 0 }} target={5} position="15" ease="elastic.out(0.1, 0.2)" />
 
-                    <Tween from={{x:'-5vw',y:0,transform:'rotate(20deg)'}} to={{ x:'10vw',y: '-55vh', transform: 'rotate(0deg)' }} target={6} position="17" ease="back.out(1.5)"/>
-                    <Tween from={{opacity:0}} to={{ opacity: 1 }} target={7} position="18" />
+                    <Tween from={{ x: '-5vw', y: 0, transform: 'rotate(20deg)' }} to={{ x: '18vw', y: '-60vh', transform: 'rotate(0deg)' }} target={6} position="17" ease="back.out(1.5)" />
+                    <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} target={7} position="18" />
+
+                    <Tween from={{ opacity: 0 }} to={{ delay: 0.5, opacity: 1 }} target={8} position="18" />
+                    <Tween from={{ opacity: 0 }} to={{ delay: 1, opacity: 1 }} target={9} position="18" />
+                    <Tween from={{ opacity: 0 }} to={{ delay: 1.5,opacity: 1 }} target={10} position="18" />
 
                 </Timeline>
             </ScrollTrigger>
