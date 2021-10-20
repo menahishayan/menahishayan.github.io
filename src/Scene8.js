263c2801301d1b6992603e324f6e11dd56db81fb
MenahiShayan <shayan1232001@yahoo.co.in>
2021-10-20 20:17:51 +0530

    scene 8

--- src/Scene8.js
+++ src/Scene8.js
@@ -14,21 +14,15 @@ const techStack = [
     ['AR', 'VR', 'Illustrator', 'Final Cut Pro', 'Cinema 4D', 'AWS'],
 ]
 
-const rowLengths = techStack.map(t => t.join(' ').length), minRL = Math.min(...rowLengths),
-    rangeFactor = (Math.max(...rowLengths) - minRL) / 10
-
-const getHoverDistance = (j) => {
-    let x = 10 + ((rowLengths[j] - minRL) / rangeFactor)
-    return (j % 2 === 0 ? -x : x) + 'vw'
-}
+const rowLengths = techStack.map(t => t.join(' ').length), minRL = Math.min(...rowLengths), rangeFactor = (Math.max(...rowLengths) - minRL) / 15
+const getHoverDistance = (j) => (j % 2 === 0 ? '-' : '') + (10 + ((rowLengths[j] - minRL) / rangeFactor)) + 'vw'
+const randoms = techStack.map(r => [...r.slice(-2), ...r, ...r.slice(0, 2)].map(_ => ({ from: (Math.random() / 6) + 0.05, to: (Math.random() / 3) + 0.1 })))
 
 const HoveringWordsBackdrop = forwardRef((props, ref) => {
-    // const title = useRef(null)
     const rows = useRef([])
     const words = useRef([...Array(props.list.length)].map((_, j) => Array(rowLengths[j])))
 
     useImperativeHandle(ref, () => {
-        // let obj = { title }
         let obj = {}
         rows.current.forEach((r, i) => obj['r' + (i + 1)] = { current: r })
         words.current.forEach((r, i) => r.forEach((w, j) => obj[`r${i + 1}w${j + 1}`] = { current: w }))
@@ -40,13 +34,12 @@ const HoveringWordsBackdrop = forwardRef((props, ref) => {
                 {props.list.map((r, j) => (
                     <div key={j} style={{ whiteSpace: 'nowrap' }} ref={d => rows.current[j] = d}>
                         {[...r.slice(-2), ...r, ...r.slice(0, 2)].map((w, i) => (
-                            <span className="word-backdrop" key={i} ref={s => words.current[j].push(s)}>{w}</span>
+                            <span className="word-backdrop" key={i} ref={s => words.current[j][i] = s}>{w}</span>
                         ))}
                         <br />
                     </div>
                 ))}
             </div>
-            {/* <div className="slide-7-statement" ref={title}>{props.title}</div> */}
         </Fragment>
     );
 });
@@ -54,21 +47,14 @@ const HoveringWordsBackdrop = forwardRef((props, ref) => {
 const Scene8 = () => (
     <Scene pin>
         <section className="slide-8" style={{ background: 'radial-gradient(circle farthest-corner at 28.7% 6.3%, #840641 0%, #320b46 90%)' }}>
-            <ScrollTrigger start="1200vh center" end="1500vh center" scrub={0.7} markers>
-                <Timeline
-                    target={
-                        <div className="slide-7-statement" >Achieved through mastery in the most number of skills you've ever seen.</div>
-                    }
-                    labels={Array(20).map((_, a) => { return { label: a + '', position: a } })}
-                >
-                    <Tween from={{ transform: 'scale(9)', opacity: 0 }} to={{ transform: 'scale(1)', opacity: 1 }} position="0" ease="back.out(0.9)" />
-                </Timeline>
+            <ScrollTrigger start="1200vh center" end="1500vh center" scrub={1}>
+                <Tween from={{ transform: 'scale(9)', opacity: 0 }} ease="back.out(0.9)" >
+                    <div className="slide-7-statement" >Achieved through mastery in the most number of skills you've ever seen.</div>
+                </Tween>
             </ScrollTrigger>
-            <ScrollTrigger start="400vh center" end="2000vh center" scrub={0.7} markers>
+            <ScrollTrigger start="400vh center" end="2200vh center" scrub={1} >
                 <Timeline
-                    target={
-                        <HoveringWordsBackdrop title="Achieved through mastery in the most number of skills you've ever seen." list={techStack} />
-                    }
+                    target={<HoveringWordsBackdrop list={techStack} />}
                     labels={Array(20).map((_, a) => { return { label: a + '', position: a } })}
                 >
                     {
@@ -76,12 +62,8 @@ const Scene8 = () => (
                             <Fragment>
                                 <Tween to={{ x: getHoverDistance(j) }} target={"r" + (j + 1)} position="0" />
                                 {
-                                    r.map((w, i) => (
-                                        <Fragment>
-                                            <Tween from={{ opacity: (Math.random() / 5) + 0.15 }} to={{ opacity: (Math.random() / 5) + 0.18 }} target={`r${j + 1}w${i + 1}`} position="0" />
-                                            {/* <Tween to={{ opacity: (Math.random() / 5) + 0.18 }} target={`r${j + 1}w${i + 1}`} position="1" />
-                                            <Tween to={{ opacity: (Math.random() / 5) + 0.18 }} target={`r${j + 1}w${i + 1}`} position="2" /> */}
-                                        </Fragment>
+                                    [...r.slice(-2), ...r, ...r.slice(0, 2)].map((w, i) => (
+                                        <Tween from={{ opacity: randoms[j][i].from }} to={{  opacity: randoms[j][i].to }} target={`r${j + 1}w${i + 1}`} position="0" />
                                     ))
                                 }
                             </Fragment>
