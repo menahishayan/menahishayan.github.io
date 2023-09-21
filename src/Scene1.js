6de94eb03e914f593946b0262051fae4904a2fef
MenahiShayan <shayan1232001@yahoo.co.in>
2023-09-21 14:22:15 +0530

    chore: update subtitle

--- src/Scene1.js
+++ src/Scene1.js
@@ -3,23 +3,21 @@ import { Tween, SplitChars, ScrollTrigger } from 'react-gsap';
 import './App.css'
 
 const Scene1 = () => (
-    <Scene pin>
-        <section className="slide-1">
-            <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5} >
-                <Tween from={{ opacity: 0 }}>
-                    <h1 className="main-title">Menahi Shayan</h1>
-                </Tween>
-            </ScrollTrigger>
+  <Scene pin>
+    <section className="slide-1">
+      <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5}>
+        <Tween from={{ opacity: 0 }}>
+          <h1 className="main-title">Menahi Shayan</h1>
+        </Tween>
+      </ScrollTrigger>
 
-            <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5}>
-                <Tween from={{ opacity: 0 }} stagger={0.1}>
-                    <SplitChars wrapper={<div className="subtitle" />}>
-                        Developer.&nbsp;&nbsp;Designer.&nbsp;&nbsp;Product Manager.
-                    </SplitChars>
-                </Tween>
-            </ScrollTrigger>
-        </section>
-    </Scene>
-)
+      <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5}>
+        <Tween from={{ opacity: 0 }} stagger={0.1}>
+          <SplitChars wrapper={<div className="subtitle" />}>Web & Mobile Engineering, UX Design, Software Architecture</SplitChars>
+        </Tween>
+      </ScrollTrigger>
+    </section>
+  </Scene>
+);
 
 export default Scene1;
