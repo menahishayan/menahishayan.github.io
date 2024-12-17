56afd35b83d678a03a5dd585bb354a33c4532426
Menahi Shayan <menahi.shayan@gmail.com>
2024-12-16 20:51:02 -0500

    chore: :memo: copy changes

--- src/Scene1.js
+++ src/Scene1.js
@@ -6,14 +6,14 @@ const Scene1 = () => (
   <Scene pin>
     <section className="slide-1">
       <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5}>
-        <Tween from={{ opacity: 0 }}>
+        <Tween from={{ opacity: 20 }}>
           <h1 className="main-title">Menahi Shayan</h1>
         </Tween>
       </ScrollTrigger>
 
       <ScrollTrigger start="80vh center" end="900vh center" scrub={0.5}>
         <Tween from={{ opacity: 0 }} stagger={0.1}>
-          <SplitChars wrapper={<div className="subtitle" />}>Web & Mobile Engineering, UX Design, Software Architecture</SplitChars>
+          <SplitChars wrapper={<div className="subtitle" />}>Product Strategy & Engineering Lead</SplitChars>
         </Tween>
       </ScrollTrigger>
     </section>
