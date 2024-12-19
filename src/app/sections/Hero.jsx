f11b2f76bf4f7fe13d1bd4b4131b55c9fd54b0d9
Menahi Shayan <menahi.shayan@gmail.com>
2024-12-19 14:44:35 -0500

    hero style

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -6,14 +6,14 @@ export default function Hero() {
   return (
     <section className="grid grid-cols-2 px-32 gap-16">
       <div>
-        <p className="font-bold">Hi, I'm</p>
+        <p className="font-bold text-lg">Hi, I'm</p>
         <h1 className="main-title">Menahi Shayan</h1>
         <p className="subtitle">
           the Founding Engineer at Zenduty, and CTO and Cofounder of Next Up. I've led engineering teams to scale SaaS products and have spoken
           internationally on what I've learnt from it.
         </p>
       </div>
-      <Image src={heroImage} width={350} height={350} alt="It's a me M...Menahi!" />
+      <Image src={heroImage} width={350} height={350} alt="It's a me M...Menahi!" className="mx-auto" />
     </section>
   );
 }
